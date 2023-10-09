aed55a6ac60acdadfe4a135b732cb3b3db786bb1
kxiside <swendside@gmail.com>
2023-10-09 12:58:13 -0700

    routes, models, middleware, json

---
+++ app/models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
+
+module.exports = mongoose.model('Product', productSchema)
